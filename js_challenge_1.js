c38cf25e5183b66d472ba994301c14a65970c182
lizzieneeley <142993029+lizzieneeley@users.noreply.github.com>
2024-05-28 14:10:19 -0400

    Create js_challenge_1.js

---
+++ js_challenge_1.js
@@ -0,0 +1,19 @@
+//ask user their name
+function userDesc(){
+let firstName=prompt ("What is your name?");
+let lastName = prompt("What is your last name?");
+// add second function here 
+
+// update the alert 
+alert("Hello " + firstName + " " + lastName);
+
+
+let age = prompt("How old are you?");
+
+if (age < 14){
+    alert("You can't apply.")
+}else{
+    alert("You can apply, welcome!")
+}
+
+}
